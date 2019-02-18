2ae4f115e3839acbd1399d4deda66b5d6dabb0c2
Vivian0430 <2522002039@qq.com>
2019-02-18 15:47:25 +0800

    second

---
+++ pages/userAddress.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "userAddress"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/userCollect.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "userCollect"
+    }
+</script>
+
+<style scoped>
+
+</style>
